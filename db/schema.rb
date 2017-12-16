6c04c110cb63bdecbf9918ede15612be92a4b728
Daryll Santos <daryll.santos@gmail.com>
2017-12-17 00:01:43 +0800

    Removed old db/schema.

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,66 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170916190959) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "activity_tags", force: :cascade do |t|
-    t.string   "name",       null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string   "shortcut"
-  end
-
-  create_table "budget_tags", force: :cascade do |t|
-    t.string   "name",       null: false
-    t.string   "shortcut",   null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "expense_budget_tags", force: :cascade do |t|
-    t.integer  "expense_id",    null: false
-    t.integer  "budget_tag_id", null: false
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-    t.index ["budget_tag_id"], name: "index_expense_budget_tags_on_budget_tag_id", using: :btree
-    t.index ["expense_id"], name: "index_expense_budget_tags_on_expense_id", using: :btree
-  end
-
-  create_table "expenses", force: :cascade do |t|
-    t.string   "description", null: false
-    t.integer  "amount",      null: false
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  create_table "pomodoro_activity_tags", force: :cascade do |t|
-    t.integer  "pomodoro_id"
-    t.integer  "activity_tag_id"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.index ["activity_tag_id"], name: "index_pomodoro_activity_tags_on_activity_tag_id", using: :btree
-    t.index ["pomodoro_id"], name: "index_pomodoro_activity_tags_on_pomodoro_id", using: :btree
-  end
-
-  create_table "pomodoros", force: :cascade do |t|
-    t.integer  "duration",    null: false
-    t.string   "description", null: false
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  create_table "weights", force: :cascade do |t|
-    t.decimal  "value",       null: false
-    t.date     "recorded_at", null: false
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  add_foreign_key "expense_budget_tags", "budget_tags"
-  add_foreign_key "expense_budget_tags", "expenses"
-  add_foreign_key "pomodoro_activity_tags", "activity_tags"
-  add_foreign_key "pomodoro_activity_tags", "pomodoros"
 end
