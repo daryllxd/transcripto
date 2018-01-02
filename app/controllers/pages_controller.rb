19decadf52e1576bd9177aa7192037bd3f27e22e
Daryll Santos <daryll.santos@gmail.com>
2018-01-03 04:38:54 +0800

    Show featured talk in the home page.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,8 @@ class PagesController < ApplicationController
   include HighVoltage::StaticPage
 
   def index
+    @talk_summaries = TalkSummary.all
+    @featured_talk_summary = TalkSummary.last
     redirect_to dashboard_path if user_signed_in?
   end
 end
