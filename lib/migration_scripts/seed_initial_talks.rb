a34324ac80850aab3153fc1b915496a0f998b5d0
Daryll Santos <daryll.santos@gmail.com>
2018-02-16 03:13:55 +0800

    Fixed bug where '\n' gets included as talk titles when seeding the initial talks.

--- lib/migration_scripts/seed_initial_talks.rb
+++ lib/migration_scripts/seed_initial_talks.rb
@@ -8,10 +8,10 @@ module MigrationScripts
       opened_file = File.read('db/seeded_talks/sandi-metz-magic-tricks-of-testing.md').split('---')
 
       talk = Talk.create!(
-        presenter: opened_file[0].remove('\n'),
-        video_url: opened_file[1].remove('\n'),
-        event: opened_file[2].remove('\n'),
-        title: opened_file[3].remove('\n')
+        presenter: opened_file[0].strip,
+        video_url: opened_file[1].strip,
+        event: opened_file[2].strip,
+        title: opened_file[3].strip
       )
 
       TalkSummary.create!(
--- spec/lib/seed_initial_talks_spec.rb
+++ spec/lib/seed_initial_talks_spec.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
-RSpec.describe SeedInitialTalks do
+RSpec.describe MigrationScripts::SeedInitialTalks do
   it 'spec_name' do
     _alice = create(:user)
 
-    SeedInitialTalks.new.seed
+    described_class.new.seed
 
     expect(TalkSummary.count).to eq(1)
+    expect(Talk.first.video_url).to eq('https://www.youtube.com/watch?v=URSWYvyc42M')
   end
 end
