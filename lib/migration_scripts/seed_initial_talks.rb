4e55872b1f76f9b5556f93b7145ea05b0979e00a
Daryll Santos <daryll.santos@gmail.com>
2018-03-15 14:33:56 +0800

    Make seeding dynamic.

--- lib/migration_scripts/seed_initial_talks.rb
+++ lib/migration_scripts/seed_initial_talks.rb
@@ -5,21 +5,25 @@ module MigrationScripts
     def seed
       first_user = User.first
 
-      opened_file = File.read('db/seeded_talks/sandi-metz-magic-tricks-of-testing.md').split('---')
+      seeded_talk_file_locations = Dir['db/seeded_talks/*.md']
 
-      talk = Talk.create!(
-        presenter: opened_file[0].strip,
-        video_url: opened_file[1].strip,
-        event: opened_file[2].strip,
-        title: opened_file[3].strip
-      )
+      seeded_talk_file_locations.each do |file_location|
+        opened_file = File.read(file_location).split('---')
 
-      TalkSummary.create!(
-        body: opened_file[4],
-        talk_id: talk.id,
-        submitted_by_user: first_user,
-        submitted_at: Date.current
-      )
+        talk = Talk.create!(
+          presenter: opened_file[0].strip,
+          video_url: opened_file[1].strip,
+          event: opened_file[2].strip,
+          title: opened_file[3].strip
+        )
+
+        TalkSummary.create!(
+          body: opened_file[4],
+          talk_id: talk.id,
+          submitted_by_user: first_user,
+          submitted_at: Date.current
+        )
+      end
     end
 
     def revert
