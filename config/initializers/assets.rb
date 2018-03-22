56a3cf417471ba13db20e17bbee1e756e127097b
Daryll Santos <daryll.santos@gmail.com>
2018-03-22 17:20:56 +0800

    Removed bower/old asset pipeline-related code.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,7 +3,3 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
-Rails.application.config.assets.paths << "#{Rails.root}/client/src"
-Rails.application.config.assets.paths << "#{Rails.root}/client/images"
-Rails.application.config.assets.paths << "#{Rails.root}/client/stylesheets"
-Rails.application.config.assets.paths << "#{Rails.root}/client/bower_components"
