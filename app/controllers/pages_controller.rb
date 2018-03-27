0e4e1b3d2a146715cc0d3b6610aa99fb4eb29ce2
Daryll Santos <daryll.santos@gmail.com>
2018-03-27 20:46:15 +0800

    Dry up views/homepage has a different layout from the other pages due to the Jumbotron.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,23 @@
 class PagesController < ApplicationController
   include HighVoltage::StaticPage
 
+  # Order actually matters, this has to be after include HighVoltage::StaticPage
+  layout :layout_for_page
+
   def index
     @talk_summaries = TalkSummary.all
     @featured_talk_summary = TalkSummary.last || NullTalkSummary.new
     redirect_to dashboard_path if user_signed_in?
+  end
+
+  private
+
+  def layout_for_page
+    case params[:action]
+    when 'index'
+      'home'
+    else
+      'application'
+    end
   end
 end
